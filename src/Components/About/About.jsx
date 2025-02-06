3872834a6995624860136a6bf501ff23428052ba
Prathyusha <shambalaprathyusha@gmail.com>
2025-02-05 21:10:47 -0800

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='about-left'>
+        <img src ={about_img} alt='' className='about-img'/>
+        <img src ={play_icon}alt=""  className='play-icon'/>
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERCITY</h3>
+        <h2>Nuturing Tomorrow's Leaders Today </h2>
+        <p>Offering diverse programs in science, technology, business, humanities, and arts.
+        Encouraging critical thinking, problem-solving, and innovation.</p>
+        <p>Extracurricular activities like student government, clubs, and organizations.
+        Public speaking, teamwork, and decision-making training.</p>
+        <p>Professors, industry leaders, and alumni networks provide career guidance.
+        Internship and research opportunities to gain real-world experience.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
